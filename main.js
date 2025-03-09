87623072614ecfb719dd66fe2bd17fd264a808a2
mahmudulashev <urgutmahmud@gmail.com>
2025-03-09 13:30:54 +0500

    Add hamburger menu for mobile navigation; implement off-screen menu and toggle functionality

--- main.js
+++ main.js
@@ -13,3 +13,13 @@ document.querySelectorAll(".faq").forEach(item => {
     });
 });
 
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreen = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');     
+    offScreen.classList.toggle('active');
+});
+
+
